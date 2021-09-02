7940c45f35ad56daf7d280ebc745a43245ce00cc
Will Soula <william.soula@starz.com>
2021-09-02 09:37:51 -0600

    fix: Put links to gifs not webpages

--- tacotuesday.py
+++ tacotuesday.py
@@ -10,8 +10,8 @@ class Tacotuesday(BotPlugin):
     def taco_tuesday(self, msg, args):
         """Send gif"""
         urls = [
-            'https://wifflegif.com/gifs/682069-taco-tuesday-lebron-james-gif',
-            'https://wifflegif.com/gifs/10653-taco-tuesday-taco-spam-gif'
+            'https://66.media.tumblr.com/f1449d030ffb2a856b2ec8fe6ece0798/tumblr_pw6x3nggXd1ue1d0so1_500.gif', # noqa
+            'https://www.icegif.com/wp-content/uploads/icegif-3463.gif'
         ]
         self.send_card(
             in_reply_to=msg,
